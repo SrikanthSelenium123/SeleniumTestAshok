1079c770524d4d1d05efb4d1a8d927b95e3d0ab8
SrikanthSelenium123 <srikanth.chincholi@gmail.com>
2023-02-06 00:12:14 +0530

    Update JSAlertClass.java

--- java_batch_1_project2/src/jsscriptexecutor/JSAlertClass.java
+++ java_batch_1_project2/src/jsscriptexecutor/JSAlertClass.java
@@ -8,12 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-/**
- *
-  * mazak testing
- * @author DELL
- *
- */
+
 
 public class JSAlertClass {
 
@@ -35,6 +30,30 @@ public class JSAlertClass {
 
 	@Test()
 	public void webTableinChrome1() throws Exception {
+		d = new ChromeDriver();
+		d.manage().window().maximize();
+		d.get("https://en.wikipedia.org/wiki/Lok_Sabha");
+		d.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+		JavascriptExecutor js = ((JavascriptExecutor) d);
+		Thread.sleep(5000);
+		js.executeScript(
+				"document.evaluate(\"//input[@name='search']\",document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue.value='Test'");
+		// WebElement ele = d.findElement(By.xpath("//input[@name='search']"));
+		// js.executeScript("arguments[0].value='Selenium'", ele);
+		Thread.sleep(5000);
+		// js.executeScript("document.getElementsByName('search')[0].value='Selenium'");
+		String str = (String) js.executeScript("return document.getElementById('searchInput').value");
+		System.out.println(str);
+		d.findElement(By.xpath("//input[@placeholder='Search Wikipedia']")).click();
+		Thread.sleep(5000);
+		WebElement search = d.findElement(By.xpath("//button[text()='Search']"));
+
+		js.executeScript("arguments[0].click();", search);
+
+	}
+	
+	@Test()
+	public void webTableinChrome2() throws Exception {
 		d = new ChromeDriver();
 		d.manage().window().maximize();
 		d.get("https://en.wikipedia.org/wiki/Lok_Sabha");

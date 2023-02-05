91ce337a3277d592aa84b79a353993a3d5070143
srikanthselenium123 <srikanth.chincholi@gmail.com>
2023-02-06 00:02:32 +0530

    added mazak

--- java_batch_1_project2/src/jsscriptexecutor/JSAlertClass.java
+++ java_batch_1_project2/src/jsscriptexecutor/JSAlertClass.java
@@ -8,7 +8,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
+/**
+ * 
+ * mazak testing
+ * @author DELL
+ *
+ */
 
 public class JSAlertClass {
 
